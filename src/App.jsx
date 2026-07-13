0a33f5eb4a1ae1e09e2ba23448991322aaa9a16e
angelwilson1007 <angelwilson1007@gmail.com>
2026-07-13 22:15:10 +0530

    view

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddCar from './components/AddCar'
+import ViewCar from './components/ViewCar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
       
       <AddCar/>
+      <ViewCar/>
     </>
   )
 }
---
+++ src/components/ViewCar.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
+
+
+const ViewCar = () => {
+
+  const [data, changeData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchData = () => {
+    axios.get("https://host-demo-app.onrender.com/api/cars").then(
+      (response) => {
+        changeData(response.data)
+        setLoading(false)
+      }
+    ).catch(
+      () => {
+        setLoading(false)
+      }
+    )
+  }
+
+  useEffect(
+    () => {
+      fetchData()
+    }, []
+  )
+
+  return (
+    <div>
+
+
+      <div className="container mt-4">
+
+        {loading ? (
+          <h3 className="text-center">Loading...</h3>
+        ) : (
+
+          <div className="table-responsive">
+
+            <table className="table table-bordered table-striped table-hover">
+
+              <thead className="table-dark">
+                <tr>
+                  <th>Reg No</th>
+                  <th>Brand</th>
+                  <th>Model</th>
+                  <th>Type</th>
+                  <th>Fuel</th>
+                  <th>Transmission</th>
+                  <th>Seats</th>
+                  <th>Rent/Day</th>
+                  <th>City</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+
+              <tbody>
+
+                {data.map((value, index) => {
+
+                  return (
+
+                    <tr key={index}>
+                      <td>{value.registration_number}</td>
+                      <td>{value.brand}</td>
+                      <td>{value.model}</td>
+                      <td>{value.vehicle_type}</td>
+                      <td>{value.fuel_type}</td>
+                      <td>{value.transmission}</td>
+                      <td>{value.seating_capacity}</td>
+                      <td>{value.rent_per_day}</td>
+                      <td>{value.city}</td>
+                      <td>{value.availability_status}</td>
+                    </tr>
+
+                  )
+
+                })}
+
+              </tbody>
+
+            </table>
+
+          </div>
+
+        )}
+
+      </div>
+
+    </div>
+  )
+}
+
+export default ViewCar
