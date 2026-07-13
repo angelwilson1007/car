eacbe1ba04e1d9682bcb7003c960831b8696f447
angelwilson1007 <angelwilson1007@gmail.com>
2026-07-13 22:02:32 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddCar from './components/AddCar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       
-      
+      <AddCar/>
     </>
   )
 }
---
+++ src/components/AddCar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+
+const AddCar = () => {
+  return (
+    <div>
+        
+
+        <div className="container">
+            <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row">
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">registration number</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">brand</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">model</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">vehicle-type</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">fuel-type</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">transmission</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">seating capacity</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">rent-per-day</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">city </label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <label htmlFor="" className="form-label">availability status</label>
+                          <input type="text" className="form-control" />
+                    </div>
+                    
+                    
+                    <div className="col-col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                         <button className="btn btn-primary">submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddCar
